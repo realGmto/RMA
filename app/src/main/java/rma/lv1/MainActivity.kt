2e23351e44961652a9f92df835ae08dcd9c91d9c
Nikola Muženjak <gmto.gtmo@hotmail.com>
2024-05-09 10:53:25 +0200

    Done LV3

--- app/src/main/java/rma/lv1/MainActivity.kt
+++ app/src/main/java/rma/lv1/MainActivity.kt
@@ -1,5 +1,6 @@
 package rma.lv1
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -34,11 +35,23 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
+import androidx.navigation.compose.rememberNavController
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import rma.lv1.ui.theme.LV1Theme
 import java.security.AllPermission
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.TriggerEvent
+import android.hardware.TriggerEventListener
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,13 +65,131 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
 
                 ) {
-                    BackgroundImage(modifier = Modifier)
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "main_screen") {
+                        composable("main_screen") {
+                            MainScreen(navController = navController)
+                        }
+                        composable("step_counter") {
+                            StepCounter(navController = navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+    @Composable
+    fun MainScreen(navController: NavController) {
+        Box(
+            modifier = Modifier.fillMaxSize(), contentAlignment =
+            Alignment.Center
+        ) {
+            BackgroundImage(modifier = Modifier.fillMaxSize())
+            UserPreview("Nikola")
+            // Button to navigate to StepCounter
+            Button(
+                onClick = {
+                    // Navigate to OtherScreen when button clicked
+                    navController.navigate("step_counter")
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Text(text = "Step Counter")
+            }
+        }
+    }
+    @Composable
+    fun StepCounter(navController: NavController) {
+        val db = Firebase.firestore
+        var steps by remember {
+            mutableStateOf(0)
+        }
+
+        db.collection("BMI").document("mKWvFApbcOYLnbnkW2vc")
+            .get()
+            .addOnSuccessListener { result ->
+                steps = result.getDouble("steps")!!.toInt()
+            }
+            .addOnFailureListener {e ->
+                Log.e("MainActivity", "Error Getting data: $e")
+            }
+
+        val sensorManager =
+            (LocalContext.current.getSystemService(Context.SENSOR_SERVICE) as
+                    SensorManager)
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
+        val triggerEventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+                    if(event?.values == null) {
+                        return
+                    }
+                    val alpha: Float = 0.8f
+                    var gravity = floatArrayOf(0f, 0f, 0f)
+                    var linear_acceleration = floatArrayOf(0f, 0f, 0f)
+
+                    gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+                    gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+                    gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+                    linear_acceleration[0] = event.values[0] - gravity[0]
+                    linear_acceleration[1] = event.values[1] - gravity[1]
+                    linear_acceleration[2] = event.values[2] - gravity[2]
+
+                    val x = linear_acceleration[0]
+                    val y = linear_acceleration[1]
+                    val z = linear_acceleration[2]
+
+                    val value = sqrt(x * x + y * y + z * z)
+                    if (value > 13) {
+                        steps++
+                        db.collection("BMI").document("mKWvFApbcOYLnbnkW2vc")
+                            .update("steps", steps)
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                // Do nothing
+            }
+        }
+        sensor?.also { light ->
+            sensorManager.registerListener(triggerEventListener, light, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+
+
+
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            BackgroundImage(modifier = Modifier.fillMaxSize())
+            Column {
+                Text(
+                    text = "Step Count",
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(text = steps.toString(),
+                    fontSize = 20.sp
+                )
+            }
+            // Back button
+            Button(
+                onClick = { navController.popBackStack() },
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(16.dp)
+            ) {
+                Text("User Info")
+            }
+        }
+    }
 
 
 @Composable
@@ -169,9 +300,7 @@ fun BackgroundImage(modifier: Modifier) {
         contentScale = ContentScale.Crop,
         alpha = 0.1F
     )
-        UserPreview(name = "Nikola", modifier = Modifier.fillMaxSize())
     }
-
 }
 @Preview(showBackground = false)
 @Composable
